b8eb352deb25e651b7b652c0a3e1e315b149458f
Alex Zloy <usvplus@gmail.com>
2025-01-22 17:09:17 +0200

    icon-update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,27 +10,19 @@ export const createImageTemplate = image => {
         />
         </a>
         <div class="image-info">
-                <p class="image-info-item"><svg class="image-icon" width="22" height="22">
-                <use href="./img/icons.svg#icon-like"></use>
-                </svg>
+                <p class="image-info-item"><b>Likes</b>
                 ${image.likes}
                 </p>
                 <p class="image-info-item">
-                <svg class="image-icon" width="22" height="22">
-                <use href="./img/icons.svg#icon-multimedia"></use>
-                </svg>
+                <b>Views</b>
                 ${image.views}
                 </p>
                 <p class="image-info-item">
-                <svg class="image-icon" width="22" height="22">
-                <use href="./img/icons.svg#icon-chat"></use>
-                </svg>
+                <b>Comments</b>
                 ${image.comments}
                 </p>
                 <p class="image-info-item">
-                <svg class="image-icon" width="22" height="22">
-                <use href="./img/icons.svg#icon-download"></use>
-                </svg>
+                <b>Downloads</b>
                 ${image.downloads}
                 </p>
         </div>
